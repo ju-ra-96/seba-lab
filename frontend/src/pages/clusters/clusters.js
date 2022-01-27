d2faed5afe1abf349d2b9a1589daefd22d0f5624
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-01-27 02:53:39 +0100

    added alert notifucation with popups

--- frontend/src/pages/clusters/clusters.js
+++ frontend/src/pages/clusters/clusters.js
@@ -1,42 +1,82 @@
-import React, { useEffect } from 'react';
-import Cluster from '../../components/cluster/cluster';
-import axios from 'axios';
-import useStore from "../../services/useStore";
-import { toast } from 'react-toastify';
-import './clusters.css';
+import React, { useEffect, useState } from 'react'
+import Cluster from '../../components/cluster/cluster'
+import axios from 'axios'
+import useStore from '../../services/useStore'
+import { ToastContainer, toast } from 'react-toastify'
+
+import { Consumer } from 'sqs-consumer'
+import AWS from 'aws-sdk'
+
+import './clusters.css'
+
+AWS.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY, region: 'us-east-1' })
 
 const Clusters = () => {
+  const clusters = useStore((state) => state.clusters)
+  const setClusters = useStore((state) => state.setClusters)
+  const deleteDialogState = useStore((state) => state.deleteDialogState)
+  const createDialogState = useStore((state) => state.createDialogState)
 
-    const clusters = useStore((state) => state.clusters);
-    const setClusters = useStore((state) => state.setClusters);
-    const deleteDialogState = useStore((state) => state.deleteDialogState);
-    const createDialogState = useStore((state) => state.createDialogState);
+  const [alert, setAlert] = useState({})
 
-    useEffect(() => {
-        getClusters();
-    }, [createDialogState,deleteDialogState]);
+  const app = Consumer.create({
+    queueUrl: 'https://sqs.eu-central-1.amazonaws.com/704085658970/alert-queue',
+    handleMessage: async (message) => {
+      console.log(message)
+      console.log('data inside')
+      const alertObj = fromStringToJson(message.Body)
+      setAlert(alertObj)
+      toast.configure()
+      toast.warn(alertObj.groupLabels.alertname)
+    },
+    sqs: new AWS.SQS(),
+  })
+  app.on('error', (err) => {
+    console.error(err.message)
+  })
 
-    const getClusters = async (e) => {
-            const axiosConfig = {
-                Headers: {
-                    "Content-Type": "application/json",
-                },
-            };
-            await axios.get('http://localhost:8000/api/cluster/getClusters', axiosConfig)
-                .then(res => {
-                    setClusters(res.data.map((cluster) => <Cluster key={cluster.id} id={cluster.id} name={cluster.name} config={cluster.config} />))
-                })   
-                .catch((e) => {
-                    toast.configure()
-                    toast.error('Error getting the clusters')
-                });;
+  app.on('processing_error', (err) => {
+    console.error(err.message)
+  })
+
+  const fromStringToJson = (message) => {
+    const jsonObj = JSON.parse(message)
+    return jsonObj
+  }
+
+  const getClusters = async (e) => {
+    const axiosConfig = {
+      Headers: {
+        'Content-Type': 'application/json',
+      },
     }
+    await axios
+      .get('http://localhost:8000/api/cluster/getClusters', axiosConfig)
+      .then((res) => {
+        setClusters(res.data.map((cluster) => <Cluster key={cluster.id} id={cluster.id} name={cluster.name} config={cluster.config} />))
+      })
+      .catch((e) => {
+        toast.configure()
+        toast.error('Error getting the clusters')
+      })
+  }
 
-    return (
-        <div className="clusters-page">
-            {clusters}
-        </div>
-    );
-};
+  console.log(alert)
+  console.log(app.isRunning)
 
-export default Clusters;
+  useEffect(() => {
+    app.start()
+    console.log(app.isRunning)
+
+    getClusters()
+  }, [createDialogState, deleteDialogState])
+
+  return (
+    <>
+      <div className='clusters-page'>{clusters}</div>
+      <ToastContainer position='bottom-right' autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+    </>
+  )
+}
+
+export default Clusters
