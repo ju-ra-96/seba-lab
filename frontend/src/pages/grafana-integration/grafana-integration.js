3d1e7a25258633300a29169f057b7a6a26c45fe4
Julian Rahimi <rahimi.julian@yahoo.de>
2022-02-03 18:47:40 +0100

    added updated link

--- frontend/src/pages/grafana-integration/grafana-integration.js
+++ frontend/src/pages/grafana-integration/grafana-integration.js
@@ -7,6 +7,7 @@ class SampleDashboard extends React.Component {
     
     clusterUrl = "http://ac7a3d8bc445d4223954b1a07fdae049-56304998.eu-central-1.elb.amazonaws.com:3000/d/B-Qbxg-nk/node-exporter-quickstart-and-dashboard?orgId=1&var-datasource=Prometheus&var-instance=" + window.location.pathname.slice(11);
     render() {
+        
         return (
             <>
                <iframe title ="frame" width="100%" height="2160" src={this.clusterUrl} />;
