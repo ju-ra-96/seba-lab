92d92e82b4d50883e67ddbe82c115dba6068fb89
Tatiana Baklanova <tatianabaklanova@MacBook-Air-von-Tanja.fritz.box>
2022-02-06 18:41:37 +0100

    Fix the metric queries

--- backend/socket/data_sourcing.js
+++ backend/socket/data_sourcing.js
@@ -32,7 +32,7 @@ async function get_cpu_usage() {
 async function get_cpu_usage_over_day_abs() {
     let metrics = {};
     for (let i = 0; i < cluster_names.length; i++) {
-        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=node_cpu_seconds_total{cluster_name="' + cluster_names[i] + '",mode="idle"}[1d:1h]';
+        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=node_cpu_seconds_total{cluster_name="' + cluster_names[i] + '",mode="idle"}[1d:2h]';
         metrics[cluster_names[i]] = await get_metrics(link, 'CPU', cluster_names[i]);
     }
     console.log('The metrics are', metrics);
@@ -41,7 +41,7 @@ async function get_cpu_usage_over_day_abs() {
 
 async function get_cpu_usage_over_day() {
     let metrics = {};
-    var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=(100-(avg%20by%20(cluster_name)%20(irate(node_cpu_seconds_total{mode="idle"}[5m]))*100))[1d:1h]';
+    var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=(100-(avg%20by%20(cluster_name)%20(irate(node_cpu_seconds_total{mode="idle"}[5m]))*100))[1d:2h]';
     metrics = await get_metrics(link, 'CPU over time', 'all clusters');
     console.log('The metrics are', metrics);
     return metrics;
@@ -51,7 +51,7 @@ async function get_cpu_usage_over_day() {
 async function get_ram_usage() {
     let metrics = {};
     for (let i = 0; i < cluster_names.length; i++) {
-        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=100*((node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"}-node_memory_MemFree_bytes{cluster_name="' + cluster_names[i] + '"})/(node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"}))';
+        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=100*((sum(node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"})-sum(node_memory_MemFree_bytes{cluster_name="' + cluster_names[i] + '"}))/(sum(node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"})))';
         metrics[cluster_names[i]] = await get_metrics(link, 'RAM', cluster_names[i]);
     }
     console.log('The metrics are', metrics);
@@ -62,7 +62,7 @@ async function get_ram_usage() {
 async function get_ram_usage_over_day() {
     let metrics = {};
     for (let i = 0; i < cluster_names.length; i++) {
-        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=(100*((node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"}-node_memory_MemFree_bytes{cluster_name="' + cluster_names[i] + '"})/(node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"})))[1d:1h]';
+        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=(100*((sum(node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"})-sum(node_memory_MemFree_bytes{cluster_name="' + cluster_names[i] + '"}))/(sum(node_memory_MemTotal_bytes{cluster_name="' + cluster_names[i] + '"}))))[1d:2h]' ;
         metrics[cluster_names[i]] = await get_metrics(link, 'RAM', cluster_names[i]);
     }
     console.log('The metrics are', metrics);
@@ -73,7 +73,7 @@ async function get_ram_usage_over_day() {
 async function get_disk_usage() {
     let metrics = {};
     for (let i = 0; i < cluster_names.length; i++) {
-        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=100-((node_filesystem_avail_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"}*100)/node_filesystem_size_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"})';
+        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=100-((sum(node_filesystem_avail_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"})*100)/sum(node_filesystem_size_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"}))';
         metrics[cluster_names[i]] = await get_metrics(link, 'Disk', cluster_names[i]);
     }
     console.log('The metrics are', metrics);
@@ -84,7 +84,7 @@ async function get_disk_usage() {
 async function get_disk_usage_over_day() {
     let metrics = {};
     for (let i = 0; i < cluster_names.length; i++) {
-        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=(100-((node_filesystem_avail_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"}*100)/node_filesystem_size_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"}))[1d:1h]';
+        var link = 'http://ade37cc803177462a8f012ed39b45f0a-1814643453.eu-central-1.elb.amazonaws.com:9090/api/v1/query?query=(100-((sum(node_filesystem_avail_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"})*100)/sum(node_filesystem_size_bytes{cluster_name="' + cluster_names[i] + '",mountpoint="/",fstype!="rootfs"})))[1d:2h]';
         metrics[cluster_names[i]] = await get_metrics(link, 'Disk', cluster_names[i]);
     }
     console.log('The metrics are', metrics);
