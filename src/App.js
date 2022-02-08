c1a2896eef2729744b57deff5c82ca9cacd1d0cc
Julian Rahimi <rahimi.julian@yahoo.de>
2022-02-08 13:25:05 +0100

    google gauge component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import GaugeChart from './GaugeChart'
+function App() {
+  return (
+    <div className="App">
+      <GaugeChart />
+    </div>
+  )
+}
+export default App
