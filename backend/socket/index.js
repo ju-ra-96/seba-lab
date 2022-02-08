2733ead9772437c10e25006b4336d40a8d70e785
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-02-08 03:20:24 +0100

    added deployment

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -1,61 +1,69 @@
-const { get_cpu_usage, get_ram_usage, get_disk_usage, get_cpu_usage_over_day_abs, get_ram_usage_over_day, get_disk_usage_over_day, get_cpu_usage_over_day, update_cluster_names } = require('./data_sourcing');
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const {
+  get_cpu_usage,
+  get_ram_usage,
+  get_disk_usage,
+  get_cpu_usage_over_day_abs,
+  get_ram_usage_over_day,
+  get_disk_usage_over_day,
+  get_cpu_usage_over_day,
+  update_cluster_names,
+} = require('./data_sourcing')
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+require('dotenv').config({ path: __dirname + '/.env' })
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-let cpu_metrics, ram_metrics, disk_metrics;
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+let cpu_metrics, ram_metrics, disk_metrics
 
 async function emit_metrics() {
-    await update_cluster_names();
-    while (true) {
-        cpu_metrics = await get_cpu_usage();
-        io.sockets.emit("CPU", cpu_metrics);
-        console.log('Emitted CPU metrics');
+  await update_cluster_names()
+  while (true) {
+    cpu_metrics = await get_cpu_usage()
+    io.sockets.emit('CPU', cpu_metrics)
+    console.log('Emitted CPU metrics')
 
-        ram_metrics = await get_ram_usage();
-        io.sockets.emit("RAM", ram_metrics);
-        console.log('Emitted RAM metrics');
+    ram_metrics = await get_ram_usage()
+    io.sockets.emit('RAM', ram_metrics)
+    console.log('Emitted RAM metrics')
 
-        disk_metrics = await get_disk_usage();
-        io.sockets.emit("Disk", disk_metrics);
-        console.log('Emitted Disk metrics');
+    disk_metrics = await get_disk_usage()
+    io.sockets.emit('Disk', disk_metrics)
+    console.log('Emitted Disk metrics')
 
-        cpu_metrics_over_time = await get_cpu_usage_over_day();
-        io.sockets.emit("CPU_over_time", cpu_metrics_over_time);
-        console.log('Emitted CPU metrics over time');
+    cpu_metrics_over_time = await get_cpu_usage_over_day()
+    io.sockets.emit('CPU_over_time', cpu_metrics_over_time)
+    console.log('Emitted CPU metrics over time')
 
-        ram_metrics_over_time = await get_ram_usage_over_day();
-        io.sockets.emit("RAM_over_time", ram_metrics_over_time);
-        console.log('Emitted RAM metrics over time');
+    ram_metrics_over_time = await get_ram_usage_over_day()
+    io.sockets.emit('RAM_over_time', ram_metrics_over_time)
+    console.log('Emitted RAM metrics over time')
 
-        disk_metrics_over_time = await get_disk_usage_over_day();
-        io.sockets.emit("Disk_over_time", disk_metrics_over_time);
-        console.log('Emitted Disk metrics over time');
+    disk_metrics_over_time = await get_disk_usage_over_day()
+    io.sockets.emit('Disk_over_time', disk_metrics_over_time)
+    console.log('Emitted Disk metrics over time')
 
-        console.log('Will wait 15 seconds');
-        await delay(15000);
-        console.log('Waited 15 seconds');
-    }
-    
+    console.log('Will wait 15 seconds')
+    await delay(15000)
+    console.log('Waited 15 seconds')
+  }
 }
 
 io.on('connection', (socket) => {
-    console.log('A new connection established with id ', socket.id);
-    socket.on('clusterUpdate', () => {
-        update_cluster_names_async()
-        console.log('Updated cluster names');
-    })
+  console.log('A new connection established with id ', socket.id)
+  socket.on('clusterUpdate', () => {
+    update_cluster_names_async()
+    console.log('Updated cluster names')
+  })
 })
 
 async function update_cluster_names_async() {
-    await update_cluster_names();
+  await update_cluster_names()
 }
 
 io.on('disconnect', (socket) => {
-    console.log('A user with id ', socket.id, 'disconnected');
+  console.log('A user with id ', socket.id, 'disconnected')
 })
 
-server.listen(4000);
-emit_metrics();
-
+server.listen(4000)
+emit_metrics()
--- frontend/src/components/dialogs/deleteDialog.js
+++ frontend/src/components/dialogs/deleteDialog.js
@@ -1,63 +1,59 @@
-import * as React from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import useStore from "../../services/useStore";
+import * as React from 'react'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Button from '@mui/material/Button'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import useStore from '../../services/useStore'
 
 export function DeleteDialog(props) {
+  const deleteDialogState = useStore((state) => state.deleteDialogState)
+  const closeDeleteDialog = useStore((state) => state.closeDeleteDialog)
+  const removeCluster = useStore((state) => state.removeCluster)
 
-    const deleteDialogState = useStore((state) => state.deleteDialogState);
-    const closeDeleteDialog = useStore((state) => state.closeDeleteDialog);
-    const removeCluster = useStore((state) => state.removeCluster);
+  const onCancel = async (e) => {
+    e.preventDefault()
+    localStorage.removeItem('deleteId')
+    localStorage.removeItem('deleteName')
+    closeDeleteDialog()
+  }
 
-    const onCancel = async (e) => {
-        e.preventDefault();
-        localStorage.removeItem('deleteId');
-        localStorage.removeItem('deleteName');
-        closeDeleteDialog();
+  const deleteCluster = async (e) => {
+    e.preventDefault()
+    let clusterToBeRemoved = localStorage.getItem('deleteId')
+    const config = {
+      Headers: {
+        'Content-Type': 'application/json',
+      },
     }
+    await axios
+      .delete('http://localhost:8000/api/cluster/deleteCluster/' + clusterToBeRemoved, config)
+      .then(removeCluster(clusterToBeRemoved), toast.configure(), toast.success('cluster deleted successfully'))
+      .catch((e) => {
+        toast.configure()
+        toast.error('Error deleting the cluster')
+      })
+    localStorage.removeItem('deleteId')
+    localStorage.removeItem('deleteName')
+    closeDeleteDialog()
+  }
 
-    const deleteCluster = async (e) => {
-        e.preventDefault();
-        let clusterToBeRemoved = localStorage.getItem('deleteId');
-        const config = {
-            Headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        await axios.delete('http://localhost:8000/api/cluster/deleteCluster/' + clusterToBeRemoved, config)
-            .then(
-                removeCluster(clusterToBeRemoved),
-                toast.configure(),
-                toast.success('cluster deleted successfully'),
-            )
-            .catch((e) => {
-                toast.configure()
-                toast.error('Error deleting the cluster')
-            });
-        localStorage.removeItem('deleteId');
-        localStorage.removeItem('deleteName');
-        closeDeleteDialog();
-    }
-
-    return <Dialog key={props.id} open={deleteDialogState} onClose={closeDeleteDialog}>
-        <DialogTitle>Delete {localStorage.getItem('deleteName')}</DialogTitle>
-        <DialogContent>
-            <DialogContentText>
-                Are you sure you would like to delete this cluster ?
-            </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={onCancel}>Cancel</Button>
-            <Button onClick={deleteCluster}>Delete</Button>
-        </DialogActions>
+  return (
+    <Dialog key={props.id} open={deleteDialogState} onClose={closeDeleteDialog}>
+      <DialogTitle>Delete {localStorage.getItem('deleteName')}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Are you sure you would like to delete this cluster ?</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel}>Cancel</Button>
+        <Button onClick={deleteCluster}>Delete</Button>
+      </DialogActions>
     </Dialog>
-};
+  )
+}
 
-export default DeleteDialog;
+export default DeleteDialog
